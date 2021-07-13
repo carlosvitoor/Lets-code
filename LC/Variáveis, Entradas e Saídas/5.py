8fcbe50b14ccd784766ec50475744350d30d6a5f
cavitso <calosvitorsmarques@gmail.com>
2021-07-13 12:43:49 -0300

    Exercicios primeira lista

---
+++ LC/Variáveis, Entradas e Saídas/5.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça as 4 notas bimestrais de um aluno
+# e mostre a média aritmética delas.
+
+n = float(input("Entre com o primeira nota:"))
+n1 = float(input("Entre com o segunda nota:"))
+n2 = float(input("Entre com o terceira nota:"))
+n3 = float(input("Entre com o quarta nota:"))
+print((n + n1 + n2 + n3)/4)
